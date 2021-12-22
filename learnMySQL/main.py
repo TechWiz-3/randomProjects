345498a71cb49844d8f7670c090c66735f3077c6
TechWiz-3 <programmingplus1@gmail.com>
2021-12-22 22:17:48 +1100

    removed the password and added env

--- learnMySQL/main.py
+++ learnMySQL/main.py
@@ -1,8 +1,14 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+password = os.getenv("password")
+
 mydb = mysql.connector.connect(
   host="containers-us-west-23.railway.app",
   user="root",
-  password="yUWgeMOav9HmGFDqUIXo",
+  password=password,#sus person, why are you reading this line??
   database="railway",
   port="6499"
 )
@@ -19,13 +25,12 @@ mycursor = mydb.cursor()
 # print(mycursor.rowcount, "record inserted.")
 
 # mycursor.execute("CREATE TABLE test_goals_2002 (id INT AUTO_INCREMENT PRIMARY KEY, user VARCHAR(255), goals VARCHAR(255))")
-person = "ctx.user"
-goal = input("What's your new years goal: ")
-# finalValues = f'"{person}", "{goal}"'
-finalValues = (person, goal)
-print(finalValues)
-sql = "INSERT INTO test_goals_2002 (user, goals) VALUES (%s, %s)"
-mycursor.execute(sql, finalValues)
-mydb.commit()
-# mycursor.execute()
+# person = "ctx.user"
+# goal = input("What's your new years goal: ")
+# finalValues = (person, goal)
+# print(finalValues)
+# sql = "INSERT INTO test_goals_2002 (user, goals) VALUES (%s, %s)"
+# mycursor.execute(sql, finalValues)
+# mydb.commit()
+# # mycursor.execute()
 
