b65ecaeeaf410f1aa4d42983f7192768a8015bb4
TechWiz-3 <programmingplus1@gmail.com>
2022-04-05 20:57:07 +1000

    more sense now

--- class_work/case_study/check_ip_port.py
+++ class_work/case_study/check_ip_port.py
@@ -18,6 +18,7 @@ import pyfiglet as ascii_text
 from datetime import datetime
 import syslog
 from os import system
+import threading
 
 # Function for writing to log file
 def log(payload):
@@ -87,13 +88,12 @@ def calculate_range(network_ip):
     for last_octet in range(255, 1, -2): # starts at 255, goes down till 1, goes down by 2
                                          # (255 not counted due to being broadcast addr)
         counter += 1
-        if counter <= 10: # first 10 ips reserved for printers and servers
+        if counter <= 5: # first 10 ips reserved for printers and servers
             pass
         else:
             full_ip = f"{network_ip}.{last_octet}"
             if host_up(full_ip): # if the host is up
-                pass
-                #scan_ports(network_ip, last_octet) # scan ports
+                scan_ports(network_ip, last_octet) # scan ports
 
 def host_up(full_addr: str): # function to determine if a host is up
     # returns true or false if the host is up or not
@@ -101,45 +101,24 @@ def host_up(full_addr: str): # function to determine if a host is up
     if system("ping -c 1 " + full_addr) == 0: # if host responds when pinged
         print("Host is up")
         return True
-    else: # ping failed, either host is down or has blocked pings
-        # try on http port
-        print("Ping failed, port connection attempt in progress")
-        port = 80
-    
-        s = socket(AF_INET, SOCK_STREAM)
-        setdefaulttimeout(1)   
-        # returns an error indicator
-        print(f"Connecting to {full_addr}:{port}")
-        result = s.connect((full_addr,port))
-        if result == None:
-            print("Host is down")
-            return False
-        elif result == 0:
-            print(f"Port {port} is open")
-            s.close()
-            return True
     return False
 
 def scan_ports(network_ip, host_ip):
     target = f"{network_ip}.{host_ip}" # defines target ip address to scan
     print(target)
-    for port in range(60000):
-        print(port)
-        try:
-            target = gethostbyname(target)
-        except Exception as error:
-            print(f"Error occurred\n{error}")
-        else:
-            print("Host name retrieved succesfully")
+    #for port in range(65535): # <-- if all ports must be scanned
+    with open("ports.txt", "r") as ports:
+        for port in ports:
+            port = int(port)
         
-        print(f"Connecting to {target}:{port}")
-        s = socket(AF_INET, SOCK_STREAM)
-        setdefaulttimeout(1)   
-        # returns an error indicator
-        result = s.connect_ex((target,port))
-        if result == 0:
-            print(f"Port {port} is open")
-        s.close()
+            print(f"Connecting to {target}:{port}")
+            s = socket(AF_INET, SOCK_STREAM)
+            setdefaulttimeout(1)   
+            # returns an error indicator
+            result = s.connect_ex((target,port))
+            if result == 0:
+                print(f"Port {port} is open")
+            s.close()
     
 
 banner = ascii_text.figlet_format("ROI PORT SCANNER")
